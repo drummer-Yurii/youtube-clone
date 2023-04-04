96118e7d3bb53c0fd06a06c0a758b2a442de0905
Yurii Rybachok <drummer2009@ukr.net>
2023-04-04 15:58:06 +0300

    create project & setup

---
+++ resources/js/Layouts/NavLayout.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+import { Link } from '@inertiajs/vue3';
+
+</script>
+
+<template>
+    <div class="relative">
+        <div id="TopNav" class="w-[100%] h-[60px] fixed bg-black z-20 flex items-center justify-between">
+
+        </div>
+        <div id="SideNav">
+
+        </div>
+        <div id="SideNavOverlay">
+
+        </div>
+
+        <slot />
+    </div>
+</template>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import NavLayout from '@/Layouts/NavLayout.vue';
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+    laravelVersion: {
+        type: String,
+        required: true,
+    },
+    phpVersion: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <Head title="Youtube" />
+
+    <NavLayout>
+        
+    </NavLayout>
+</template>
+
