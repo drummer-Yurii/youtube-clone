f18d05414ac758acde55a5515034bc723117c843
Yurii Rybachok <drummer2009@ukr.net>
2023-04-04 17:02:23 +0300

    side navigation

--- resources/js/Layouts/NavLayout.vue
+++ resources/js/Layouts/NavLayout.vue
@@ -3,6 +3,9 @@ import { ref } from 'vue';
 import { Link } from '@inertiajs/vue3';
 import MenuIcon from 'vue-material-design-icons/Menu.vue';
 import MagnifyIcon from 'vue-material-design-icons/Magnify.vue';
+import SideNavItem from '@/Components/SideNavItem.vue';
+
+let openSideNav = ref(true);
 
 </script>
 
@@ -10,7 +13,7 @@ import MagnifyIcon from 'vue-material-design-icons/Magnify.vue';
     <div class="relative">
         <div id="TopNav" class="w-[100%] h-[60px] fixed bg-black z-20 flex items-center justify-between">
             <div class="flex items-center">
-                <button class="p-2 ml-3 rounded-full hover:bg-gray-800 inline-block cursor-pointer">
+                <button @click="openSideNav = !openSideNav" class="p-2 ml-3 rounded-full hover:bg-gray-800 inline-block cursor-pointer">
                     <MenuIcon fillColor="#FFFFFF" :size="26" />
                 </button>
                 <div class="mx-2"></div>
@@ -59,8 +62,36 @@ import MagnifyIcon from 'vue-material-design-icons/Magnify.vue';
                 />
             </div>
         </div>
-        <div id="SideNav">
-
+        <div
+            id="SideNav"
+            class="h-[100%] fixed z-0 bg-black"
+            :class="[!openSideNav ? 'w-[70px]' : 'w-[240px]']"
+        >
+            <ul :class="[!openSideNav ? 'p-2' : 'px-5 pb-2 pt-[7px]']" class="mt-[60px] w-full">
+                <SideNavItem :openSideNav="openSideNav" iconString="Home" />
+                <SideNavItem :openSideNav="openSideNav" iconString="Add Video" />
+                <SideNavItem :openSideNav="openSideNav" iconString="Delete Video" />
+                <div class="border-b border-b-gray-700 my-2.5"></div>
+                <SideNavItem :openSideNav="openSideNav" iconString="Subscriptions" />
+                <SideNavItem :openSideNav="openSideNav" iconString="Library" />
+                <SideNavItem :openSideNav="openSideNav" iconString="Liked" />
+                <SideNavItem :openSideNav="openSideNav" iconString="History" />
+                <SideNavItem :openSideNav="openSideNav" iconString="Watch Later" />
+                <div v-if="openSideNav">
+                    <div class="border-b border-b-gray-700 my-2.5" />
+                    <div class="text-gray-400 text-[14px] text-extrabold">
+                        About Press Copyring
+                        <div>Contact us</div>
+                        Creator Advertise Developers
+                    </div>
+                    <div class="border-b border-b-gray-700 my-2.5"></div>
+                    <div class="text-gray-400 text-[14px] text-extrabold">
+                        Terms Privacy & Safety
+                        <div>How Youtube works</div>
+                        <span>Test new features</span>
+                    </div>
+                </div>
+            </ul>
         </div>
         <div id="SideNavOverlay">
 
